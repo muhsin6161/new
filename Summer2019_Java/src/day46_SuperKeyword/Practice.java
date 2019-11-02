bcb86daf358d56c59fbd23c7f7cb7296cc8cfa29
muhsin6161 <muhsin_mutlu61@hotmail.com>
2019-11-02 13:25:48 -0400

    m

---
+++ Summer2019_Java/src/day46_SuperKeyword/Practice.java
@@ -0,0 +1,25 @@
+package day46_SuperKeyword;
+class parent {
+	
+	public parent(int a) {
+		System.out.println("A");
+	}
+	
+	public parent(String b) {
+		System.out.println("C");
+	}
+	
+}
+public class Practice extends parent {
+
+	public Practice() {
+		super("A");
+	}
+	
+	public static void main(String[] args) {
+		Practice obj = new Practice();
+		
+	}
+	
+	
+}
